72aab79f814c2e20f7304150f2ceb9961663052a
mulgogi <teikim040215@naver.com>
2023-07-27 13:24:32 +0900

    헤더까지 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니!
 // 장바구니 토글만들기
 // 웹 페이지에 존재하는 html 요소에 접근하고자 할 때는 반드시 document 객체부터 시작해야함
 const basketStarterEl = document.querySelector("header .basket-starter");
@@ -21,3 +22,43 @@ window.addEventListener("click", function () {
   basketEl.classList.remove("show");
 });
 // 장바구니 토글창 영역 이외의 영역을 클릭했을떄 토글창(?) 닫기
+
+// 검색!
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...document.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
+
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transtitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transtitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+  setTimeout(function () {
+    searchInputEl.focus();
+  }, 600);
+}
+function hideSearch() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transtitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transtitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+  searchDelayEls.reverse();
+  searchInputEl.velue = " ";
+}
